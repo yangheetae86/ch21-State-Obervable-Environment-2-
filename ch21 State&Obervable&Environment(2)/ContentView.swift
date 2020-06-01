a733ed109a980e94a23d28bf88d2004ada058227
heroparkjisung2 <heroparkjisung2@gmail.com>
2020-06-01 15:14:11 +0900

    Initial Commit

---
+++ ch21 State&Obervable&Environment(2)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ch21 State&Obervable&Environment(2)
+//
+//  Created by HEE TAE YANG on 2020/06/01.
+//  Copyright © 2020 yht. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
